4069cc34dd8424fb68cb1060adf08ced9745ed89
Juan Antonio Orozco bianchi <iam@mexcoder.com>
2020-08-23 18:20:18 -0500

    added the extractors to the main script

--- featuresExtractor.py
+++ featuresExtractor.py
@@ -1,6 +1,21 @@
 from basicFeatures import LenExtractor
+from UrlParsedFeatures import AtSymbolExtractor, DashExtractor
+from UrlParsedFeatures import HttpsTokenExtractor, IpExtractor
+from UrlParsedFeatures import NonStandarPortExtractor, NumberSubdomainsExtractor
+from UrlParsedFeatures import RedirectExtractor, UrlShortenedExtractor
+from AlexaParsedFeatures import AlexaRankExtractor
+from WebClientFeatures import  HttpsStatusExtractor,FavIconStatusExtractor
+from WebClientFeatures import  RequestURLExtractor,anchorURLExtractor
+from WebClientFeatures import  LinksInMetaExtractor,ServerFromHandlerExtractor
+from WebClientFeatures import  MailToInServerFromHandlerExtractor,IframeExtractor
 
-extractors = [LenExtractor]
+
+extractors = [LenExtractor, AtSymbolExtractor, DashExtractor,HttpsTokenExtractor,
+              IpExtractor, NonStandarPortExtractor, NumberSubdomainsExtractor,
+              RedirectExtractor, UrlShortenedExtractor, AlexaRankExtractor,
+              HttpsStatusExtractor,FavIconStatusExtractor,RequestURLExtractor,
+              anchorURLExtractor,LinksInMetaExtractor,ServerFromHandlerExtractor,
+              MailToInServerFromHandlerExtractor, IframeExtractor]
 
 nameMapping = {"Length" : "URL_Length"}
 
@@ -20,5 +35,11 @@ def extractFeatures(target):
     return resultingFeatures
 
 if __name__ == "__main__":
+    import sys
+    
     testString = "https://test.com"
+
+    if len(sys.argv) > 2:
+        testString = sys.argv[1]
+
     print (extractFeatures(testString))
--- tagger.py
+++ tagger.py
@@ -5,5 +5,9 @@ def getTagAndFeatures(target):
     return (target, extractFeatures(target))
 
 if __name__ == "__main__":
+    import sys
     testString = "https://test.com"
+
+    if len(sys.argv) > 2:
+        testString = sys.argv[1]
     print (getTagAndFeatures(testString))
